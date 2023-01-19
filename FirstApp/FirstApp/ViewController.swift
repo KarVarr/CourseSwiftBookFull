def5a24fe603f53f5816e32f28f63be6b4822c3c
KarVarr <megatr9n@gmail.com>
2023-01-19 21:36:55 +0300

    added button

--- FirstApp/FirstApp/ViewController.swift
+++ FirstApp/FirstApp/ViewController.swift
@@ -10,10 +10,13 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+            super.viewDidLoad()
+        
     }
 
-
+    @IBAction func buttonTapped2(_ sender: UIButton) {
+        
+    }
+    
 }
 
