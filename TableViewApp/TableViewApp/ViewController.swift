72ceba39de9bc485834c79fb6f322f846dad4e81
KarVarr <megatr9n@gmail.com>
2023-01-20 17:38:11 +0300

    added tableView and cell

--- TableViewApp/TableViewApp/ViewController.swift
+++ TableViewApp/TableViewApp/ViewController.swift
@@ -7,11 +7,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+  
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Hello world!"
+        
+        return cell
     }
 
 
