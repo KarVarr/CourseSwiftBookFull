67f8fc2761cf027f0df1fe8f2c6ffefb93f1b46a
KarVarr <megatr9n@gmail.com>
2023-01-21 21:38:49 +0300

    added delete func UITableViewCell.EditinStyle

--- Eateries/Eateries/EateriesTableViewController.swift
+++ Eateries/Eateries/EateriesTableViewController.swift
@@ -8,37 +8,37 @@
 import UIKit
 
 class EateriesTableViewController: UITableViewController {
-
-    let restaurantsNames = ["balkan","bochka","bonsai","dastarhan","elu","indokitay","istorii","klassik","love","morris","ogonek","respublika","shok","speakeasy","x.o"]
-       
-       let restaurantesImages = ["balkan","bochka","bonsai","dastarhan","elu","indokitay","istorii","klassik","love","morris","ogonek","respublika","shok","speakeasy","x.o"]
+    
+    var restaurantsNames = ["balkan","bochka","bonsai","dastarhan","elu","indokitay","istorii","klassik","love","morris","ogonek","respublika","shok","speakeasy","x.o"]
+    
+    var restaurantesImages = ["balkan","bochka","bonsai","dastarhan","elu","indokitay","istorii","klassik","love","morris","ogonek","respublika","shok","speakeasy","x.o"]
     
     var restaurantIsVisited = [Bool](repeatElement(false, count: 15))
     
-    let phoneNumbers = ["+1 304 333 86", "+1 456 443 33", "+1 123 345 55","+1 777 366 84","+1 304 456 83","+1 388 888 36","+1 311 323 81","+1 545 563 44","+1 987 87 87","+1 666 567 56","+1 877 908 66","+1 304 300 98","+1 456 333 86","+1 304 456 12","+1 304 987 00",]
+    var phoneNumbers = ["+1 304 333 86", "+1 456 443 33", "+1 123 345 55","+1 777 366 84","+1 304 456 83","+1 388 888 36","+1 311 323 81","+1 545 563 44","+1 987 87 87","+1 666 567 56","+1 877 908 66","+1 304 300 98","+1 456 333 86","+1 304 456 12","+1 304 987 00",]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return restaurantsNames.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! EateriesTableViewCell
@@ -55,7 +55,7 @@ class EateriesTableViewController: UITableViewController {
         return cell
     }
     
-   
+    
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let ac = UIAlertController(title: nil, message: "Choose action", preferredStyle: .actionSheet)
@@ -72,9 +72,9 @@ class EateriesTableViewController: UITableViewController {
             let cell = tableView.cellForRow(at: indexPath)
             self.restaurantIsVisited[indexPath.row] = !self.restaurantIsVisited[indexPath.row]
             cell?.accessoryType = self.restaurantIsVisited[indexPath.row] ? .checkmark : .none
-          
-        }
             
+        }
+        
         let cancel = UIAlertAction(title: "Cancel", style: .cancel)
         
         ac.addAction(call)
@@ -82,5 +82,18 @@ class EateriesTableViewController: UITableViewController {
         ac.addAction(cancel)
         present(ac, animated: true)
     }
-
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle,
+        forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            self.restaurantesImages.remove(at: indexPath.row)
+            self.restaurantsNames.remove(at: indexPath.row)
+            self.restaurantIsVisited.remove(at: indexPath.row)
+            self.phoneNumbers.remove(at: indexPath.row)
+        }
+//        tableView.reloadData()
+        tableView.deleteRows(at: [indexPath], with: .fade)
+    }
+    
+    
 }
