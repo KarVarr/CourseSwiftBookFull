d720b702ec8a98739cbe070addb7852b428bac30
KarVarr <megatr9n@gmail.com>
2023-01-21 20:29:43 +0300

    added selectionStyle and accessoryType

--- Eateries/Eateries/EateriesTableViewController.swift
+++ Eateries/Eateries/EateriesTableViewController.swift
@@ -13,6 +13,8 @@ class EateriesTableViewController: UITableViewController {
        
        let restaurantesImages = ["balkan","bochka","bonsai","dastarhan","elu","indokitay","istorii","klassik","love","morris","ogonek","respublika","shok","speakeasy","x.o"]
     
+    var restaurantIsVisited = [Bool](repeatElement(false, count: 15))
+    
     let phoneNumbers = ["+1 304 333 86", "+1 456 443 33", "+1 123 345 55","+1 777 366 84","+1 304 456 83","+1 388 888 36","+1 311 323 81","+1 545 563 44","+1 987 87 87","+1 666 567 56","+1 877 908 66","+1 304 300 98","+1 456 333 86","+1 304 456 12","+1 304 987 00",]
     
     override func viewDidLoad() {
@@ -46,7 +48,10 @@ class EateriesTableViewController: UITableViewController {
         cell.thumbnailImageVIew.contentMode = .scaleAspectFill
         cell.nameLabel.text = restaurantsNames[indexPath.row].capitalized
         
-
+        cell.accessoryType = self.restaurantIsVisited[indexPath.row] ? .checkmark : .none
+        
+        cell.selectionStyle = .none
+        
         return cell
     }
     
@@ -54,15 +59,19 @@ class EateriesTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let ac = UIAlertController(title: nil, message: "Choose action", preferredStyle: .actionSheet)
+        
         let call = UIAlertAction(title: "Call \(phoneNumbers[indexPath.row])", style: .default) { ac in
             let ac = UIAlertController(title: "Warning", message: "You can't call on this phone number", preferredStyle: .alert)
             ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
             self.present(ac, animated: true)
         }
         
-        let isVisited = UIAlertAction(title: "Pin Place", style: .default) { ac in
+        let isVisitedTitle = self.restaurantIsVisited[indexPath.row] ?  "Delete Pin" : "Pin Place"
+        
+        let isVisited = UIAlertAction(title: isVisitedTitle, style: .default) { ac in
             let cell = tableView.cellForRow(at: indexPath)
-            cell?.accessoryType = .checkmark 
+            self.restaurantIsVisited[indexPath.row] = !self.restaurantIsVisited[indexPath.row]
+            cell?.accessoryType = self.restaurantIsVisited[indexPath.row] ? .checkmark : .none
           
         }
             
