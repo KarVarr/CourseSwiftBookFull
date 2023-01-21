9aefa3dcdbfc45f493fbcc73af8491b09e0585a7
KarVarr <megatr9n@gmail.com>
2023-01-21 16:54:33 +0300

    added images folder and first images from app

--- TableViewApp/TableViewApp/ViewController.swift
+++ TableViewApp/TableViewApp/ViewController.swift
@@ -9,13 +9,12 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    let restaurants = ["balkan","bochka","bonsai","check-icon-green","dastarhan","elu","indokitay","istorii","klassik","love","morris","ogonek","respublika","shok","speakeasy","x.o"]
+    let restaurants = ["balkan","bochka","bonsai","dastarhan","elu","indokitay","istorii","klassik","love","morris","ogonek","respublika","shok","speakeasy","x.o"]
     
-    let restaurantesImages = ["balkan","bochka","bonsai","check-icon-green","dastarhan","elu","indokitay","istorii","klassik","love","morris","ogonek","respublika","shok","speakeasy","x.o"]
+    let restaurantesImages = ["balkan","bochka","bonsai","dastarhan","elu","indokitay","istorii","klassik","love","morris","ogonek","respublika","shok","speakeasy","x.o"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
     
     
@@ -25,6 +24,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.font = UIFont(name: "Arial", size: 24)
         cell.textLabel?.text = restaurants.sorted()[indexPath.row].capitalized
         cell.imageView?.image = UIImage(named: restaurantesImages[indexPath.row])
         return cell
