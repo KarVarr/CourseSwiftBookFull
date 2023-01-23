6f7382fdbff7c55aaf170c807bbd934de05b340e
KarVarr <megatr9n@gmail.com>
2023-01-23 21:20:39 +0300

    added UITableViewDelegate and UITableViewDataSourse

--- Eateries/Eateries/Detail/EateriesDetailViewController.swift
+++ Eateries/Eateries/Detail/EateriesDetailViewController.swift
@@ -7,9 +7,11 @@
 
 import UIKit
 
-class EateriesDetailViewController: UIViewController {
-
+class EateriesDetailViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    
+
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var imageView: UIImageView!
     var imageName = ""
     
@@ -22,15 +24,18 @@ class EateriesDetailViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! EateryDetailTableViewCell
+        
+        cell.keyValue.text = "some text"
+        cell.valueLabel.text = "some value text"
+        
+        return cell
+    }
 
 }
 
