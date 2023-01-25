4f57606527928165f739232b1ff921804a6daf48
KarVarr <megatr9n@gmail.com>
2023-01-25 18:43:18 +0300

    resolve issue Thread 1: EXC_BAD_ACCESS and added view programmatically to the simulator

--- UIKitProgrammatically/UIKitProgrammatically/ViewController.swift
+++ UIKitProgrammatically/UIKitProgrammatically/ViewController.swift
@@ -8,10 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var view1: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .red
+        
+        return view
+    }()
+    
+    var view2: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .blue
+        
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(view1)
+        view.addSubview(view2)
+        
+        setupView1()
+        setupView2()
+    }
+    
+    func setupView1()  {
+        view1.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        view1.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1/3).isActive = true
+        view1.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        view1.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+    
+    func setupView2()  {
+    
     }
 
 
