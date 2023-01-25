605ff35bec3f7a4e72d8641316c29b4bd4875184
KarVarr <megatr9n@gmail.com>
2023-01-25 19:00:24 +0300

    added 2 views and 1 button wonly using code

--- UIKitProgrammatically/UIKitProgrammatically/ViewController.swift
+++ UIKitProgrammatically/UIKitProgrammatically/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var view1: UIView = {
+    let view1: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
         view.backgroundColor = .red
@@ -17,12 +17,20 @@ class ViewController: UIViewController {
         return view
     }()
     
-    var view2: UIView = {
+    let view2: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
         view.backgroundColor = .blue
         
         return view
+    }()
+    
+    let button: UIButton = {
+       let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("PUSH", for: .normal)
+        button.backgroundColor = .magenta
+        return button
     }()
 
     override func viewDidLoad() {
@@ -30,20 +38,35 @@ class ViewController: UIViewController {
         
         view.addSubview(view1)
         view.addSubview(view2)
+        view.addSubview(button)
         
         setupView1()
         setupView2()
+        setupButton()
     }
     
     func setupView1()  {
-        view1.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        view1.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
         view1.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1/3).isActive = true
         view1.heightAnchor.constraint(equalToConstant: 100).isActive = true
         view1.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
     
     func setupView2()  {
+        view2.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
+        view2.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1/3).isActive = true
+        view2.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        view2.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        //view2.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        
+    }
     
+    func setupButton() {
+        button.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -200).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        //button.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
     }
 
 
