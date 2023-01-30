c811ca05f1de29137c3c8f62ff60efb53fca832b
KarVarr <megatr9n@gmail.com>
2023-01-30 15:00:19 +0300

    added bluring background

---
+++ Eateries/Eateries/RateViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RateViewController.swift
+//  Eateries
+//
+//  Created by Karen Vardanian on 30.01.2023.
+//
+
+import UIKit
+
+class RateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        let blurEffect = UIBlurEffect(style: .light)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.frame = self.view.bounds
+        blurEffectView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        self.view.insertSubview(blurEffectView, at: 1)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
