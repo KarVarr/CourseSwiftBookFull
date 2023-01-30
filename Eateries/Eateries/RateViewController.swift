701e910f662b236afc36ca3b6ff53c010b650d6e
KarVarr <megatr9n@gmail.com>
2023-01-30 17:03:10 +0300

    added animate for buttons

--- Eateries/Eateries/RateViewController.swift
+++ Eateries/Eateries/RateViewController.swift
@@ -9,9 +9,34 @@ import UIKit
 
 class RateViewController: UIViewController {
 
+    @IBOutlet weak var ratingStackView: UIStackView!
+    @IBOutlet weak var badButton: UIButton!
+    @IBOutlet weak var goodButton: UIButton!
+    @IBOutlet weak var brilliantButton: UIButton!
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+//        UIView.animate(withDuration: 0.4) {
+//            self.ratingStackView.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+//
+//        }
+        let buttonArray = [badButton, goodButton, brilliantButton]
+        
+        
+        for (index, button) in buttonArray.enumerated() {
+            UIView.animate(withDuration: 0.4, delay: Double(index) * 0.2, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.3, options: .curveEaseIn) {
+                button?.transform = CGAffineTransform(scaleX: 1, y: 1)
+            }
+        }
+        
+
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let buttonArray = [badButton, goodButton, brilliantButton]
+        buttonArray.map{$0?.transform = CGAffineTransform(scaleX: 0, y: 0)}
         // Do any additional setup after loading the view.
         
         let blurEffect = UIBlurEffect(style: .light)
